46cd7802e6d2d020c2115adc1547c8eb2f4d67ac
Andreas Kosmowicz <andreas@kosmoskosmos.de>
2020-01-09 00:11:16 +0100

    avid blocking

--- config/synochat.php
+++ config/synochat.php
@@ -1,1 +1,11 @@
 <?php
+
+return [
+
+    /**
+     * Set this to 0|false if you want to DDNS your DiskStation,
+     * or if you're not running with fail2ban.
+     * Declared in minutes
+     **/
+     "time_to_wait_before_retry_after_failed" => 30
+];
--- src/SynochatServiceProvider.php
+++ src/SynochatServiceProvider.php
@@ -3,7 +3,16 @@
 
 namespace KosmosKosmos\SynoChat;
 
+use Illuminate\Support\ServiceProvider;
 
-class SynochatServiceProvider {
+class SynochatServiceProvider extends ServiceProvider {
+
+    public function boot() {
+
+        $this->publishes([
+             __DIR__ . '/../config/synochat.php' => config_path('synochat.php'),
+        ], 'config');
+
+    }
 
 }
